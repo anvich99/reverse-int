91afa73116201194acf4bd0ca2d45e8ec3444424
Nastya <zhitnikovich3308@gmail.com>
2023-02-11 15:19:19 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = '';
+    n = String(n)
+    let num = Number(n)
+    if(num < 0){
+        num = num * (-1);
+        n = String(num)
+    }
+    for(let i = n.length-1; i>=0; i--){
+        str+=n[i]
+    }
+    return str;
 }
